bc662d29f53bdc47f3830999d95e41cb071af302
chvrry <chvrry69@gmail.com>
2024-10-05 12:29:45 -0500

    prueba2

--- app_gestion.sql
+++ app_gestion.sql
@@ -5,6 +5,21 @@
 -- Servidor: 127.0.0.1
 -- Tiempo de generación: 31-08-2024 a las 23:29:48
 -- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `app_gestion`
+--
 
 -- --------------------------------------------------------
 
